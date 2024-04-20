b351f7aec099680d191e4ebb5732b2a74c099b50
Deni Gunawan <denigunawan775@gmail.com>
2024-04-20 23:27:15 +0700

    initial commit app pembayaran

---
+++ app/Controllers/SppController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class SppController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
