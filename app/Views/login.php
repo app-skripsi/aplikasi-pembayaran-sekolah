9b7a661ec3e53f25b185c0f77613990f3d718305
Deni Gunawan <denigunawan775@gmail.com>
2024-07-05 15:42:18 +0700

    'update'

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,7 +1,6 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
 <!DOCTYPE html>
 <html>
@@ -15,8 +14,6 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
         integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <style>
-        /* Made with love by Mutiullah Samim*/
-
         @import url('https://fonts.googleapis.com/css?family=Numans');
 
         html,
